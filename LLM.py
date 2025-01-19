2ff11871bb713c19382bb5bde4c5826441110a27
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-19 16:52:37 +0530

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -8,11 +8,15 @@ from langchain.chains import RetrievalQA
 import sentence_transformers
 os.environ['HUGGINGFACEHUB_API_TOKEN']='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'
 h=HuggingFaceHub(repo_id='google/flan-t5-large',model_kwargs={'temperature':0.9})
-loader=CSVLoader(file_path='Cleaned_Ecommerce_FAQs.csv',source_column='Question')
-data=loader.load()
 embeddings=HuggingFaceEmbeddings()
-db=FAISS.from_documents(documents=data,embedding=embeddings)
-r=db.as_retriever()
+file_path='faiss_index'
+def create_db():
+  loader=CSVLoader(file_path='Cleaned_Ecommerce_FAQs.csv',source_column='Question')
+  data=loader.load()
+  db=FAISS.from_documents(documents=data,embedding=embeddings)
+  db.save_local(file_path)
+vector_db=FAISS.load_local(file_path,embeddings)
+r=vector_db.as_retriever()
 chain=RetrievalQA.from_chain_type(llm=h,chain_type='stuff',retriever=r,input_key='query',return_source_documents=True)
 q=st.text_input()
 if q:
