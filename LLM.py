8083b86fe64adc7752974fe30683945ed7ddac79
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-19 16:41:09 +0530

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -5,6 +5,7 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
+import sentence_transformers
 os.environ['HUGGINGFACEHUB_API_TOKEN']='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'
 h=HuggingFaceHub(repo_id='google/flan-t5-large',model_kwargs={'temperature':0.9})
 loader=CSVLoader(file_path='Cleaned_Ecommerce_FAQs.csv',source_column='Question')
